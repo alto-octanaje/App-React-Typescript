0a65fe7b04fa028d46e8e7f2c91d7118ae08daa1
jeisson-olwen <jeissonolwen@gmail.com>
2023-04-26 12:08:31 -0500

    Save-

---
+++ src/types.d.ts
@@ -0,0 +1,14 @@
+import { type } from "os";
+
+export interface Sub {
+  nick: string;
+  subMonths: number;
+  avatar: string;
+  description?: string;
+}
+export type SubsResponseFromApi = Array<{
+  name: string;
+  id: number;
+  image: string;
+  created: string;
+}>
